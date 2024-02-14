f0275c7a1259f07e4748a39cbd70982c4a952df4
Martin Belej <=>
2024-02-14 12:31:32 +0100

    first

--- android/src/com/spseke/splhun/AndroidLauncher.java
+++ android/src/com/spseke/splhun/AndroidLauncher.java
@@ -4,14 +4,22 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.badlogic.gdx.physics.box2d.Box2D;
+import com.badlogic.gdx.physics.box2d.World;
 import com.spseke.splhun.MyGdxGame;
 
 public class AndroidLauncher extends AndroidApplication {
+
+
+	World world;
+
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new MyGdxGame(), config);
+
+
 	}
 
 
